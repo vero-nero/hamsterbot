fcc3c9c83d0e0bc757cc161cd3cd880c000cc6bb
vero-nero <martinrustemi505@gmail.com>
2023-04-14 20:52:50 +0200

    still error with joining, added quiz

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
+import asyncio
 import json
 import discord
 from discord.ext import commands
 import random
-description = '''An example bot to showcase the discord.ext.commands extension
-module.
-There are a number of utility commands being showcased here.'''
+import requests
+description = '''The Best Hamster'''
 
 intents = discord.Intents.default()
 intents.members = True
@@ -13,35 +13,60 @@ intents.message_content = True
 
 bot = commands.Bot(command_prefix='>', description=description, intents=intents)
 
-#@bot.event
-#async def on_ready():
-#    print(f'Logged in as {bot.user} (ID: {bot.user.id})')
-#    print('------')
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user} (ID: {bot.user.id})')
+    print('------')
 
 @bot.event
-async def on_member_joined(ctx, member = discord.Member):
+async def on_member_join(member):
     """Says when a member joined."""
-    #sus⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀ 
     amongsus ='''   
         ⠀⠀⠀⠀⠀⠀⠀⣠⣤⣤⣤⣤⣤⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀ 
         ⠀⠀⠀⠀⠀⢰⡿⠋⠁⠀⠀⠈⠉⠙⠻⣷⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀ 
         ⠀⠀⠀⠀⢀⣿⠇⠀⢀⣴⣶⡾⠿⠿⠿⢿⣿⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀ 
         ⠀⠀⣀⣀⣸⡿⠀⠀⢸⣿⣇⠀⠀⠀⠀⠀⠀ ⠙⣷⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀ 
         ⠀⣾⡟⠛⣿⡇⠀⠀⢸⣿⣿⣷⣤⣤⣤⣤⣶⣶⣿⠇⠀⠀⠀⠀⠀⠀⠀⣀⠀⠀  
-        ⢀⣿⠀⢀⣿⡇⠀⠀⠀⠻⢿⣿⣿⣿⣿⣿⠿⣿⡏⠀⠀⠀⠀⢴⣶⣶⣿⣿⣿⣆ Welcome''' + f'{member.mention}'+ ''' ⠀⠀ 
+        ⢀⣿⠀⢀⣿⡇⠀⠀⠀⠻⢿⣿⣿⣿⣿⣿⠿⣿⡏⠀⠀⠀⠀⢴⣶⣶⣿⣿⣿⣆ Welcome''' + f'{discord.Member}'+ ''' ⠀⠀ 
         ⢸⣿⠀⢸⣿⡇⠀⠀⠀⠀⠀⠈⠉⠁⠀⠀ ⣿⡇⣀⣠⣴⣾⣮⣝⠿⠿⠿⣻⡟ 
         ⢸⣿⠀⠘⣿⡇⠀⠀⠀⠀⠀⠀⠀⣠⣶⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠁⠉⠀ 
         ⠸⣿⠀⠀⣿⡇⠀⠀⠀⠀⠀⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠟⠉⠀⠀⠀⠀ 
         ⠀⠻⣷⣶⣿⣇⠀⠀⠀⢠⣼⣿⣿⣿⣿⣿⣿⣿⣛⣛⣻⠉⠁⠀⠀⠀⠀⠀⠀⠀ 
         ⠀⠀⠀⠀⢸⣿⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀ ⠀⠀ 
-        ⠀⠀⠀⠀⢸⣿⣀⣀⣀⣼⡿⢿⣿⣿⣿⣿⣿⡿⣿⣿⣿'''
-    await ctx.send(amongsus)
+        ⠀⠀⠀⠀⢸⣿⣀⣀⣀⣼⡿⢿⣿⣿⣿⣿⣿⡿⣿⣿⡿'''
+    with open('serverID.json') as f:
+        serverJson = json.load(f)
+        serverID = serverJson['id']
+    print(serverID)
+    channel = await bot.fetch_channel(serverID) 
+    print(channel)
+    await channel.send(amongsus)
 
 @bot.command()
 async def greet(ctx):
     """says hello."""
     await ctx.send("Hello!")
 
+@bot.command()
+async def listcommands(ctx):
+    commands = '''Commands with syntax:
+    >add [number] [number]
+    >sub [number] [number]
+    >mult [number] [number]
+    >div [number] [number]
+    >roll [number]d[number]
+    >choose [option] [option] [option] ...
+    >randomize [option] [option] [option] ...
+    >gaydar [option] [option] [option] ...
+    >greet
+    >listcommands
+    >help
+    >ping
+    >repeat [message] [number]
+    >repeat [message] [number] [number]
+    >quote
+    '''
+    await ctx.send(commands)
 @bot.command()
 async def add(ctx, left: int, right: int):
     """Adds two numbers together."""
@@ -127,6 +152,42 @@ async def isracism(ctx):
 @bot.command()
 async def pinging(ctx, member: discord.Member):
         await ctx.send(f'Pinging {member.mention}...')
+
+@bot.command()
+async def quizzMe(ctx):
+    response = requests.get("https://officeapi.dev/api/quotes/random")
+    json_data = json.loads(response.text)
+    if 'data' in json_data and 'content' in json_data['data']:
+        quote = json_data['data']['content']
+        character = json_data['data']['character']['firstname'] + " " + json_data['data']['character']['lastname']
+        await ctx.send(quote)
+        await ctx.send("Who said this? You have unlimited tries for 10 seconds ill")
+    else:
+        print("Error: Could not retrieve quote")
+#wait for the next message and compare it to character
+    def check(m):
+        return m.content == character and m.channel == ctx.channel
+    try:
+        msg = await bot.wait_for('message', timeout=10.0, check=check)
+        #catch any exceptions
+    except:
+        await ctx.send('You took too long.')
+    else:
+        await ctx.send('You got it!')
+@bot.command()
+#gambling command
+async def gamble(ctx, amount: int):
+    #check if the user has enough money
+    if amount > money[ctx.author.id]:
+        await ctx.send("You don't have enough money")
+    else:
+        #check if the user wins
+        if random.randint(0, 100) > 50:
+            money[ctx.author.id] += amount
+            await ctx.send("You won!")
+        else:
+            money[ctx.author.id] -= amount
+            await ctx.send("You lost!")
 
 #read the token from the bottoken.json and run the bot
 with open('bottoken.json') as f:
